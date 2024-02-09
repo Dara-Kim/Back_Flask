06630973c2df45cd47377b3bad09aa15d7477fbe
daunique <daeunkim2580@gmail.com>
2024-02-10 01:52:47 +0900

    REFACTOR: Issue #0 /auths 거치는 url 수정

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def register_router(flask_app: Flask):
     from router.auths.auths_router import auths_router
 
-    flask_app.register_blueprint(auths_router, url_prefix='/auths')
+    flask_app.register_blueprint(auths_router)
     
 def create_app():
     app = Flask(__name__)
--- Backend/manage.py
+++ Backend/manage.py
@@ -5,18 +5,3 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-
-@app.route("/predict" , methods=["GET", "POST"])
-# @app.route("/predict", methods=["POST"])
-def predict():
-    if request.method == "POST": 
-            message = {
-                "name" : "post"
-            }
-            return message
-    if request.method == "GET":
-            message = {
-                "name" : "get"
-            }
-            return message
