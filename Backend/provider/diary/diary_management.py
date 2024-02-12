2e6a1d4d06eecb51ef7642ce93ce8de9eea0cec7
daunique <daeunkim2580@gmail.com>
2024-02-12 19:50:00 +0900

    FEAT: parent, child 일기 작성 코드 추가, api랑 s3 연결해야함

--- Backend/provider/diary/diary_management.py
+++ Backend/provider/diary/diary_management.py
@@ -1,14 +1,31 @@
-# from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename
 
 
-# def writing_parent_diary(pid, text, image):
-#     imageUrl = './tmp_s3/' + secure_filename(image.filename)
-#     image.save(imageUrl)
+def writing_parent_diary(pid, text, image):
+    # imageUrl = f'http://yourserver.com/{filename}'
+    root = "./Backend/provider/diary/tmp_s3/"
+    imageUrl = root + image.filename
+    image.save(imageUrl)
 
-#     # 일기 교정 api
-#     correctedText = 'correctedText'
+    # diary correcting api
+    correctedText = "correctedText"
 
-#     # 일기 번역 api
-#     translatedText = 'translatedText'
+    # diary translating api
+    translatedText = "translatedText"
 
-#     return correctedText, translatedText, imageUrl
+    return correctedText, translatedText, imageUrl
+
+
+def writing_child_diary(pid, image):
+    # imageUrl = f'http://yourserver.com/{filename}'
+    root = "./Backend/provider/diary/tmp_s3/"
+    imageUrl = root + image.filename
+    image.save(imageUrl)
+
+    # diary correcting api
+    correctedText = "correctedText"
+
+    # diary translating api
+    translatedText = "translatedText"
+
+    return correctedText, translatedText, imageUrl
--- Backend/router/diary/diary_router.py
+++ Backend/router/diary/diary_router.py
@@ -1,20 +1,43 @@
-# from flask import Blueprint, request, jsonify
-# from provider.diary.diary_management import writing_parent_diary
+from flask import Blueprint, request, jsonify
+from provider.diary.diary_management import writing_parent_diary
+from provider.diary.diary_management import writing_child_diary
 
-# diary_router = Blueprint('/home', __name__)
+from werkzeug.utils import secure_filename
+import os
+
+diary_router = Blueprint("/home", __name__)
 
 
-# @diary_router.route('/home/parent', methods=['POST'])
-# def handle_writing_parent_diary():
-#     # Extracting pId, text and image from request
-#     pid = request.json.get('pid')
-#     text = request.json.get('text')
-#     image = request.files.get('image')
-#     # Calling function "writing_parent_diary"
-#     correctedText, translatedText, imageUrl = \
-#         writing_parent_diary(pid, text, image)
-#     return jsonify({
-#         "correctedText": correctedText,
-#         "translatedText": translatedText,
-#         "imageUrl": imageUrl
-#         })
+@diary_router.route("/home/parent", methods=["POST"])
+def handle_writing_parent_diary():
+    # Extracting pId, text and image from request
+    pid = request.form.get("pid")
+    text = request.form.get("text")
+    image = request.files["image"]
+
+    # Calling function "writing_parent_diary"
+    correctedText, translatedText, imageUrl = writing_parent_diary(pid, text, image)
+    return jsonify(
+        {
+            "correctedText": correctedText,
+            "translatedText": translatedText,
+            "imageUrl": imageUrl,
+        }
+    )
+
+
+@diary_router.route("/home/child", methods=["POST"])
+def handle_writing_child_diary():
+    # Extracting pId, text and image from request
+    pid = request.form.get("pid")
+    image = request.files["image"]
+
+    # Calling function "writing_child_diary"
+    correctedText, translatedText, imageUrl = writing_child_diary(pid, image)
+    return jsonify(
+        {
+            "correctedText": correctedText,
+            "translatedText": translatedText,
+            "imageUrl": imageUrl,
+        }
+    )
