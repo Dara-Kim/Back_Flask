d4c9160d4160b57a5f7fbe70343bb600a467887c
daunique <daeunkim2580@gmail.com>
2024-02-14 22:01:13 +0900

    REFACTOR: 로그인 구조 수정, 역할 명시,  response 코드 추후 추가

--- Backend/router/auths/auths_router.py
+++ Backend/router/auths/auths_router.py
@@ -1,18 +1,19 @@
 from flask import Blueprint, request, jsonify
-from provider.auths.login import login
+from provider.auths.login import LoginService
 
-auths_router = Blueprint('auths', __name__)
+auths_router = Blueprint("auths", __name__)
 
 
-@auths_router.route('/logIn', methods=['POST'])
+@auths_router.route("/logIn", methods=["POST"])
 def handle_login():
-    # Extracting Id and Password from request
-    username = request.json.get('username')
-    password = request.json.get('password')
-    # Calling function "login"
-    isSuccess, code, message = login(username, password)
-    # Returning response
-    if isSuccess:
-        return jsonify({"pid": 1})
-    else:
-        return code, message
+    username = request.json.get("username")
+    password = request.json.get("password")
+
+    Login = LoginService(username, password)
+    pid = Login.login()
+
+    return jsonify(
+        {
+            "pid": pid,
+        }
+    )
